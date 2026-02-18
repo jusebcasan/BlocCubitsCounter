f00e97652e909c84bd6ae08e6ddb38608f505949
jusebcasan <sebascardo15@gmail.com>
2026-02-17 20:46:58 -0500

    creo el widgets de formulario, y se dejo abierto a su utilizacion

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forms_app/presentation/widgets/widgets.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -17,6 +18,89 @@ class _RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              FlutterLogo(size: 100),
+              _RegisterForm(),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _RegisterForm extends StatefulWidget {
+  const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          CustomTextFormField(
+            label: 'Nombre de Usuario',
+            onChanged: (value) => username = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo Requerido';
+              if (value.trim().isEmpty) return 'Campo Requerido';
+              if (value.length < 6) return 'Mas de 6 letras';
+              return null;
+            },
+          ),
+          SizedBox(height: 10),
+          CustomTextFormField(
+            label: 'Correo Electronico',
+            onChanged: (value) => email = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo Requerido';
+              if (value.trim().isEmpty) return 'Campo Requerido';
+              if (value.length < 6) return 'Mas de 6 letras';
+              final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+              if (!emailRegExp.hasMatch(value))
+                return 'No tiene formato de correo';
+              return null;
+            },
+          ),
+          SizedBox(height: 20),
+          CustomTextFormField(
+            label: 'Contraseña',
+            obscureText: true,
+            onChanged: (value) => password = value,
+            validator: (value) {
+              if (value == null || value.isEmpty) return 'Campo Requerido';
+              if (value.trim().isEmpty) return 'Campo Requerido';
+              if (value.length < 6) return 'Mas de 6 letras';
+              return null;
+            },
+          ),
+          SizedBox(height: 20),
+          FilledButton.tonalIcon(
+            onPressed: () {
+              final isValid = _formKey.currentState!.validate();
+              if (!isValid) return;
+              print('$username, $email, $password');
+            },
+            label: Text("Crear Usuario"),
+            icon: Icon(Icons.save),
+          ),
+        ],
+      ),
+    );
   }
 }
