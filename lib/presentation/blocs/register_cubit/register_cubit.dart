9744c2373acf125c13bdc0e32f855934e7a0eccd
jusebcasan <sebascardo15@gmail.com>
2026-02-23 21:48:44 -0500

    se creo el cubit de register

---
+++ lib/presentation/blocs/register_cubit/register_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterFormState> {
+  RegisterCubit() : super(RegisterFormState());
+  void onSubmit() {
+    print('submit: $state');
+  }
+
+  void usernameChanged(String value) {
+    emit(state.copyWith(username: value));
+  }
+
+  void emailChanged(String value) {
+    emit(state.copyWith(email: value));
+  }
+
+  void passwordChanged(String value) {
+    emit(state.copyWith(password: value));
+  }
+}
